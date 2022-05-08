ee2d7543ef0d1c4424e5ae360ae724a3e77ba210
Christine Rowland <fordo29@gmail.com>
2022-05-07 19:17:18 -0600

    Add initial build out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <object data="logo.svg" width="43" height="42" aria-labelledby='logo'></object>
       </header>
+      <main className='racing-form'>
+        <label classname='race-time-box'>Race Time</label>
+        <div>
+          <input></input>
+          <button>Submit</button>
+        </div>
+        <hr />
+        <label className='results'>Results</label>
+      </main>
     </div>
   );
 }
