7bf5417b349d8eefae8ffca180cece5abd22d296
Christine Rowland <fordo29@gmail.com>
2022-05-08 18:45:44 -0600

    Add basic testing for the application

---
+++ cypress/integration/landingpage_spec.js
@@ -0,0 +1,19 @@
+describe('Landing page user flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('should have a label, an input box, a submit button and a section for results', () => {
+    cy.get('.racing-form').contains('Race Time')
+    cy.get('.input-box')
+    cy.get('button').contains('Submit')
+    cy.get('.results').contains('Results')
+  })
+
+  it('should be able to enter race end times, click submit and see a result', () => {
+    cy.get('.input-box').should('be.visible').type('02:00 PM, DAY 19, 02:00 PM, DAY 20, 01:58 PM, DAY 20')
+    cy.get('button').contains('Submit').click()
+    cy.get('.display-results').contains('The average race time was 27239 minutes')
+  })
+
+})
